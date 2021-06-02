bf956c958f1f05e226fcc25affb6c8f2a68917a9
Gagan Diwan <gagan.diwan@icloud.com>
2021-06-02 21:55:42 +0300

    Initial Commit

---
+++ CardPlans/CardPlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardPlansApp.swift
+//  CardPlans
+//
+//  Created by GD on 2.6.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardPlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardPlans/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardPlans
+//
+//  Created by GD on 2.6.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
